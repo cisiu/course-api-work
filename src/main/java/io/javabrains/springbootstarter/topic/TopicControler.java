00f8ac6ed366a48d84284b470aff2dab5524d5c6
cisiu <kamilcisu@gmail.com>
2018-10-16 11:16:10 +0200

    add filtes

--- src/main/java/io/javabrains/springbootstarter/topic/TopicControler.java
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicControler.java
@@ -3,17 +3,27 @@ package io.javabrains.springbootstarter.topic;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicControler {
 	
 	@RequestMapping("/topics")
-	public List<Topic> getAllTopics() {
+	public List<Topic> getAllTopics(@RequestParam(value ="name", defaultValue = "defaulty") String imie) {
 		return Arrays.asList(
 				new Topic("python", "programming in pthn"),
-				new Topic("java", "programming in pthn"),
+				new Topic(imie, "programming in pthn"),
+				new Topic("c#", "programming in c#"));
+	}
+	
+	@RequestMapping("/topics2/{name}")
+	public List<Topic> getAllTopics2(@PathVariable("name") String imie) {
+		return Arrays.asList(
+				new Topic("python", "programming in pthn"),
+				new Topic(imie, "programming in pthn"),
 				new Topic("c#", "programming in c#"));
 	}
 
